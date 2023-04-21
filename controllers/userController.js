4644760ff333b584d4f631ef7382308875b539a6
altavada <117209509+altavada@users.noreply.github.com>
2023-04-21 19:43:06 -0400

    created user controllers

---
+++ controllers/userController.js
@@ -0,0 +1,46 @@
+const User = require("../models/User");
+
+module.exports = {
+  getUsers(req, res) {
+    User.find()
+      .then((users) => res.json(users))
+      .catch((err) => res.status(500).json(err));
+  },
+  getSingleUser(req, res) {
+    User.findOne({ _id: req.params.userId })
+      .select("-__v")
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: "User not found" })
+          : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+  createUser(req, res) {
+    User.create(req.body)
+      .then((data) => res.json(data))
+      .catch((err) => res.status(500).json(err));
+  },
+  updateUser(req, res) {
+    User.findOneAndUpdate(
+      { _id: req.params._id },
+      { $set: req.body },
+      { runValidators: true, new: true }
+    )
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: "User not found" })
+          : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+  deleteUser(req, res) {
+    User.findOneAndDelete({ _id: req.params._id })
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: "User not found" })
+          : res.json({ message: `Deleted: ${user}` })
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+};
