3523e0b1b29baece80c6cd763ea0c687229954f5
altavada <117209509+altavada@users.noreply.github.com>
2023-04-21 21:50:28 -0400

    created thoughts / reactions routes

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -1,5 +1,95 @@
-const Thought = require("../models/Thought");
+const { Thought, User } = require("../models");
 
 module.exports = {
-    get
-}
+  getThoughts(req, res) {
+    Thought.find()
+      .then((thoughts) => res.json(thoughts))
+      .catch((err) => res.status(500).json(err));
+  },
+  getSingleThought(req, res) {
+    Thought.findOne({ _id: req.params.thoughtId })
+      .then((thought) =>
+        !thought
+          ? res.status(404).json({ message: "Thought not found" })
+          : res.json(thought)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+  createThought(req, res) {
+    Thought.create(req.body)
+      .then((thought) => {
+        return User.findOneAndUpdate(
+          { _id: req.body.userId },
+          { $addToSet: { thoughts: thought._id } },
+          { new: true }
+        );
+      })
+      .then((user) =>
+        !user
+          ? res.status(404).json({
+              message: "Thought created, but valid user ID not provided.",
+            })
+          : res.json("Thought created")
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+  updateThought(req, res) {
+    Thought.findOneAndUpdate(
+      { _id: req.params.thoughtId },
+      { $set: req.body },
+      { runValidators: true, new: true }
+    )
+      .then((thought) =>
+        !thought
+          ? res.status(404).json({ message: "Thought not found" })
+          : res.json(thought)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+  deleteThought(req, res) {
+    Thought.findOneAndRemove({ _id: req.params.thoughtId })
+      .then((thought) =>
+        !thought
+          ? res.status(404).json({ message: "Thought not found" })
+          : User.findOneAndUpdate(
+              { thoughts: req.params.thoughtId },
+              { $pull: { thoughts: req.params.thoughtId } },
+              { new: true }
+            )
+      )
+      .then((user) =>
+        !user
+          ? res.status(404).json({
+              message: "Thought removed but no associated user found.",
+            })
+          : res.json({ message: "Thought removed" })
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+  createThoughtReaction(req, res) {
+    Thought.findOneAndUpdate(
+      { _id: req.params.thoughtId },
+      { $addToSet: { reactions: req.body } },
+      { runValidators: true, new: true }
+    )
+      .then((thought) =>
+        !thought
+          ? res.status(404).json({ message: "Thought not found" })
+          : res.json(thought)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+  deleteThoughtReaction(req, res) {
+    Thought.findOneAndUpdate(
+      { _id: req.params.thoughtId },
+      { $pull: { reactions: { _id: req.params.reactionId } } },
+      { runValidators: true, new: true }
+    )
+      .then((thought) =>
+        !thought
+          ? res.status(404).json({ message: "Thought not found" })
+          : res.json(thought)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+};
---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,24 @@
+const router = require("express").Router();
+const {
+  getThoughts,
+  getSingleThought,
+  createThought,
+  updateThought,
+  deleteThought,
+  createThoughtReaction,
+  deleteThoughtReaction,
+} = require("../../controllers/thoughtController");
+
+router.route("/").get(getThoughts).post(createThought);
+
+router
+  .route("/:thoughtId")
+  .get(getSingleThought)
+  .put(updateThought)
+  .delete(deleteThought);
+
+router.route("/:thoughtId/reactions").post(createThoughtReaction);
+
+router.route("/:thoughtId/reactions/:reactionId").delete(deleteThoughtReaction);
+
+module.exports = router;
